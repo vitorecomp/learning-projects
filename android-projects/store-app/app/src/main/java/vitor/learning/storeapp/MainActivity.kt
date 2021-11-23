7d330941fab9c704bf464aed7ee7d84acfbac77e
vitor <vitor.ecomp@gmail.com>
2021-11-23 02:34:52 -0300

    Understatings of fragments

---
+++ android-projects/store-app/app/src/main/java/vitor/learning/storeapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package vitor.learning.storeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = Navigation.findNavController(this, R.id.main_fragment)
+        bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_menu)
+        bottomNavigationView.setupWithNavController(navController)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
+    }
+}
